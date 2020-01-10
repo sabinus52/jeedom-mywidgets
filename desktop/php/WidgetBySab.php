c724fb53d82605ac866e90cbe1ded4249c9ce946
Sabinus52 <sabinus52@gmail.com>
2020-01-10 14:15:22 +0100

    Suppression d'affichage inutile

--- desktop/php/WidgetBySab.php
+++ desktop/php/WidgetBySab.php
@@ -11,11 +11,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
    <div class="col-xs-12 eqLogicThumbnailDisplay">
   <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
   <div class="eqLogicThumbnailContainer">
-      <div class="cursor eqLogicAction logoPrimary" data-action="add">
+    <!-- <div class="cursor eqLogicAction logoPrimary" data-action="add">
         <i class="fas fa-plus-circle"></i>
         <br>
         <span>{{Ajouter}}</span>
-    </div>
+    </div> -->
       <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
       <i class="fas fa-wrench"></i>
     <br>
